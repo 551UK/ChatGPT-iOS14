9516786e98e060ca5438555bb4e168a2934c65c7
551 <wdf@hotmail.co.uk>
2026-09-11 17:17:21 +0100

    Wire live voice controller into app

--- App/ChatGPT14.m
+++ App/ChatGPT14.m
@@ -4,10 +4,11 @@
 #import <Speech/Speech.h>
 #import <AVFoundation/AVFoundation.h>
 #import <WebKit/WebKit.h>
-
+#import <math.h>
 
 #include "Parts/Core.inc"
 #include "Parts/API.inc"
+#include "Parts/Realtime.inc"
 #include "Parts/Views.inc"
 #include "Parts/ChatA.inc"
 #include "Parts/ChatB.inc"
